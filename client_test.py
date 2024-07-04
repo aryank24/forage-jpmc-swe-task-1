6d1e2eea3dbfd44e91f4faeb08e1e908510f4096
aryank24 <aryan.kaushal24@gmail.com>
2024-07-03 23:07:32 -0400

    Finished Task 1

--- client_test.py
+++ client_test.py
@@ -1,5 +1,5 @@
 import unittest
-from client3 import getDataPoint
+from client3 import getDataPoint, getRatio
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -8,6 +8,11 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      self.assertEqual(getDataPoint(quote), (quote['stock'], 
+                                             float(quote['top_bid']['price']), 
+                                             float(quote['top_ask']['price']), 
+                                             ((float(quote['top_bid']['price']) + float(quote['top_ask']['price'])) / 2)))
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,10 +20,19 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      self.assertEqual(getDataPoint(quote), (quote['stock'], 
+                                             float(quote['top_bid']['price']), 
+                                             float(quote['top_ask']['price']), 
+                                             ((float(quote['top_bid']['price']) + float(quote['top_ask']['price'])) / 2)))
 
 
   """ ------------ Add more unit tests ------------ """
-
+  def test_getRatio_priceBNonZero(self):
+    self.assertEqual(getRatio(125, 100), 1.25)
+  
+  def test_getRatio_priceBZero(self):
+    self.assertIsNone(getRatio(100, 0))
 
 
 if __name__ == '__main__':
